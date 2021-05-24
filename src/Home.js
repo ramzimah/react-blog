e2b2bcfbe34aea89585eb27040363193b0d967d6
ramzimah <ramzimahnoune@gmail.com>
2021-05-24 20:39:25 +0200

    added useFetch hook

--- src/Home.js
+++ src/Home.js
@@ -1,25 +1,13 @@
 import { useState, useEffect } from "react";
+import useFetch from "./useFetch";
 import BlogList from "./BlogList";
+
 const Home = () => {
-  const [blogs, setBlogs] = useState(null);
-  const [isPending, setIsPending] = useState(true);
-  const [error, setError] = useState(null);
-
-  useEffect(() => {
-    fetch("http://localhost:8000/blogs")
-      .then((res) => {
-        if (!res.ok) {
-          throw Error("could not fetch the data");
-        }
-        return res.json();
-      })
-      .then((data) => {
-        setBlogs(data);
-        setIsPending(false);
-      })
-      .catch((err) => setError(err.message));
-  }, []);
-
+  const {
+    data: blogs,
+    error,
+    isPending,
+  } = useFetch("http://localhost:8000/blogs");
   return (
     <div className="home">
       {error && <div>{error}</div>}
