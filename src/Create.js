18308d3e1b7ff526c06ab3171d0c7f05275c7500
ramzimah <ramzimahnoune@gmail.com>
2021-05-26 16:56:58 +0200

    added create route

--- src/Create.js
+++ src/Create.js
@@ -1,7 +1,57 @@
+import { useState } from "react";
+
 const Create = () => {
+  const [title, setTitle] = useState("");
+  const [body, setBody] = useState("");
+  const [author, setAuthor] = useState("ramzi");
+  const [isPending, setIsPending] = useState(false);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const blog = { title, body, author };
+    setIsPending(true);
+    fetch("http://localhost:8000/blogs", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(blog),
+    }).then(() => {
+      console.log("new blog added");
+      setIsPending(false);
+    });
+  };
   return (
     <div className="create">
-      <h2>add new blog</h2>
+      <form onSubmit={handleSubmit}>
+        <label>Blog title:</label>
+        <input
+          type="text"
+          value={title}
+          onChange={(e) => {
+            setTitle(e.target.value);
+          }}
+          required
+        ></input>
+        <label>Blog body:</label>
+        <textarea
+          value={body}
+          onChange={(e) => {
+            setBody(e.target.value);
+          }}
+          required
+        ></textarea>
+        <label>Blog author:</label>
+        <select
+          value={author}
+          onChange={(e) => {
+            setAuthor(e.target.value);
+          }}
+        >
+          <option value="ramzi"> ramzi</option>
+          <option value="ramzimh">ramzimh</option>
+        </select>
+        {!isPending && <button>add blog</button>}
+        {isPending && <button>adding blog...</button>}
+      </form>
     </div>
   );
 };
